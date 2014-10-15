2c1460dbb2689c196599cc6ccc1ae757c3f0325f
Matt Cobb <contact@matthewcobb.co.uk>
2014-10-15 17:38:30 +0100

    Completed Header

--- config.rb
+++ config.rb
@@ -1,24 +1,20 @@
 # Require any additional compass plugins here.
-require "susy"
-require "breakpoint"
-
-
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
+css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "img"
-javascripts_dir = "js"
-fonts_dir = "fonts"
+javascripts_dir = "javascripts"
 
-output_style = :compressed
-environment = :development
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
-color_output = false
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
